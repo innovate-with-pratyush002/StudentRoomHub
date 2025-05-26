03ee9c25487cae1bcb86afec06488fd160af2813
PRATYUSH KUMAR NISHAD <harityush2015@gmail.com>
2025-05-27 01:40:12 +0530

    add public folder for adding css and javaScript

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 const Listing= require("./models/listing.js")
 const path = require("path");
 const methodOverride= require("method-override");
-const ejs_mate = require("ejs-mate");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
-app.ejs_mate('ejs', ejs_mate);
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 const MONGO_URL="mongodb://127.0.0.1:27017/RoomForU";
