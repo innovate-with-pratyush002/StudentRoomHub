ba07b8a6f9d69f3312443fc45a603fefec2a08b6
PRATYUSH KUMAR NISHAD <harityush2015@gmail.com>
2025-06-15 23:58:08 +0530

    add validations

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing= require("./models/listing.js")
 const path = require("path");
 const methodOverride= require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync= require("./utils/wrapAsync.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); 
