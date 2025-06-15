68620f72dbf082fc10589381bf9c8d371a715298
PRATYUSH KUMAR NISHAD <harityush2015@gmail.com>
2025-06-16 00:46:01 +0530

    adding WrapAsync and ExpressError for error handling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const methodOverride= require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync= require("./utils/wrapAsync.js");
+const ExpressError= require("./utils/ExpressError.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); 
@@ -34,60 +35,93 @@ app.listen(3000,()=>{
 
 
 //home route
-app.get("/",async(req,res)=>{
+app.get("/",wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({});
     res.render("./listings/home.ejs",{allListings});
-});
+}));
 
 
 //route for showing all listings:=>
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({});
     res.render("./listings/listing.ejs",{ allListings });
-});
+}));
 
 //add route where we can add a new listing:=> 
 app.get("/listings/new",(req,res)=>{
     res.render("listings/addListing.ejs");
 });
 
-app.post("/listings",async(req,res)=>{
+app.post("/listings",wrapAsync(async(req,res)=>{
     const newData = new Listing( req.body.place);
     await newData.save();
     res.redirect("/listings");
-});
+}));
 
 
 //edit & update route:=>
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
      let {id}=req.params;
     const Data= await Listing.findById(id);
     res.render("./listings/edit.ejs",{ Data });
-});
-app.put("/listings/:id",async(req,res)=>{
+}));
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
     let{id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.place});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 
 //delete route:=>
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
       let{id}=req.params;
       await Listing.findByIdAndDelete(id);
       res.redirect("/listings");
-});
+}));
 
 
 //detail route:=>
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const Data= await Listing.findById(id);
     res.render("./listings/detail.ejs",{ Data });
+}));
+
+
+// app.get("*",(req,res,next)=>{
+// res.send("somethink went wrong");
+// });
+
+
+// middleware for error handling
+app.use((err,req,res,next)=>{
+    let{status=500,message="Something Went Wrong!"}=err;
+    res.status(status).send(message); 
 });
 
-// MAI BAS TUMHARE BURE VAKT KA EK SAHARA THA, YE GALAT FAHMI THI MUJHE KI MAI TUMHARA THA.
-// NIND TO USS DIN TUTI HAMARI JAB TERE ISHQ KE PANNE PAR NAAA KAHI JIKRA HAMARA THA.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TERI AANKHO KI SHOKHI ME KOYI RAAJ TO HOGA,
+// BINA KAHE HI SAB KUCHH KAH JANA ANDAAJ TO HOGA.
+// JHEEL SI GAHRAI, CHAND SI ROSHANI HAI INME,
+// INN NIGAAHO ME CHHUPA KOYI SAAJ TO HOGA.
+// JAB BHI DEKHU KHUD KO KHOYA PATA HU,
+// INN AANKHO ME KOYI MERA AAJ TO HOGA.
 
 
 
