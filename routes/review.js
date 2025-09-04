d0141f535dd6c61e27047c1b43f84ca6d64accbe
PRATYUSH KUMAR NISHAD <harityush2015@gmail.com>
2025-09-04 23:52:09 +0530

    create review.js file in controller for better clarity

--- routes/review.js
+++ routes/review.js
@@ -5,28 +5,14 @@ const ExpressError= require("../utils/ExpressError.js");
 const Review= require("../models/review.js");
 const router= express.Router({mergeParams: true});
 const {isLoggedIn, isReviewOwner} = require("../middleware.js");
+const ListingControllers= require("../controller/review.js");
 
 
 
 //review route
-router.post("/",isLoggedIn,wrapAsync(async(req,res)=>{
-    let listing= await Listing.findById(req.params.id);
-    let newReview= new Review(req.body.review);
-    newReview.owner= req.user._id;
-    listing.reviews.push(newReview); 
-    await newReview.save();
-    await listing.save();
-        req.flash("success","New Review Added!");
-    res.redirect(`/listings/${listing.id}`);
-}));
+router.post("/",isLoggedIn,wrapAsync(ListingControllers.createReview));
 
 //delete review route
-router.delete("/:reviewId",isLoggedIn,isReviewOwner,wrapAsync(async(req,res)=>{
-    let{id,reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-        req.flash("success","Review is Deleted!");
-    res.redirect(`/listings/${id}`);
-}));
+router.delete("/:reviewId",isLoggedIn,isReviewOwner,wrapAsync(ListingControllers.deleteReview));
 
 module.exports=router;
